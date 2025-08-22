f734b38ca5cacf8585cb5a5cfd9919bf8522cdde
doanh.doquoc <doanh.doquoc@powergatesoftware.com>
2025-08-22 09:26:23 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import {
   LineChart,
   Line,
